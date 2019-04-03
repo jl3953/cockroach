184bc8f772a357d957b72fa7f4f1e89c4c09f136
Jeffrey Helt <jmhelt13@gmail.com>
2019-04-03 18:53:02 -0400

    Add kv bench to driver script.
    Release note: None

--- experiments/driver.py
+++ experiments/driver.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 import subprocess
 import sys
 import time
 
+# Constants
 NODES = ["192.168.1.2", "192.168.1.3", "192.168.1.4"]
 EXE = "/usr/local/temp/go/src/github.com/cockroachdb/cockroach/cockroach"
 STORE_DIR = "/data"
 
-# TPCC
-DURATION_S = 30
-N_WAREHOUSES = 100
+FPATH = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.join(FPATH, '..')
+LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+
+EXP = {
+    "out_dir": os.path.join(LOGS_DIR, "kv"),
+    "coordinator": NODES[0],
+    "benchmark": "kv",
+    "duration": 30,
+    "distribution": {
+        "type": "zipf",
+        "params": {
+            "skew": 1
+        }
+    }
+}
 
 
 def call(cmd, err_msg):
@@ -77,13 +92,49 @@ def run_bench(name, args=""):
     return call(cmd, "Failed to run benchmark")
 
 
-def run_tpcc(cr_ip, duration_s, n_warehouses):
+def run_tpcc(config):
+    ip = config["coordinator"]
+    n_warehouses = config["warehouses"]
+
     name = "tpcc"
-    args = "--warehouses={0} 'postgresql://root@{1}:26257?sslmode=disable'".format(n_warehouses, cr_ip)
+    args = "'postgresql://root@{0}:26257?sslmode=disable' --warehouses={1}".format(ip, n_warehouses)
     init_bench(name, args)
 
-    args = "--warehouses={0} --ramp=30s --duration={1}s --split --scatter 'postgresql://root@{2}:26257?sslmode=disable'".format(n_warehouses, duration_s, cr_ip)
+    duration = config["duration"]
+
+    args = "'postgresql://root@{0}:26257?sslmode=disable' --warehouses={1} --ramp=30s --duration={2}s --split --scatter ".format(ip, n_warehouses, duration)
     run_bench(name, args)
+
+
+def run_kvbench(config):
+    ip = config["coordinator"]
+
+    name = "kv"
+    args = "'postgresql://root@{0}:26257?sslmode=disable'".format(ip)
+    init_bench(name, args)
+
+    duration = config["duration"]
+    distribution = config["distribution"]
+
+    args = "'postgresql://root@{0}:26257?sslmode=disable' --duration={1}s".format(ip, duration)
+    if distribution["type"] == "zipf":
+        args = "{0} --zipfian={1}".format(args, distribution["skew"])
+
+    out = run_bench(name, args)
+    out_dir = config["out_dir"]
+    path = os.path.join(out_dir, "out.txt")
+    with open(path, "w") as f:
+        f.write(out)
+
+
+def save_params(exp_params, out_dir):
+    params = {
+        "exp_params": exp_params
+    }
+
+    path = os.path.join(out_dir, "params.json")
+    with open(path, "w") as f:
+        json.dump(params, f, indent=4)
 
 
 def main():
@@ -94,11 +145,21 @@ def main():
     if args.kill:
         kill_cluster()
     else:
-        kill_cluster()
 
+        out_dir = EXP["out_dir"]
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        save_params(EXP, out_dir)
+
+        kill_cluster()
         start_cluster()
 
-        run_tpcc(NODES[0], DURATION_S, N_WAREHOUSES)
+        bench = EXP["benchmark"]
+        if bench == "tpcc":
+            run_tpcc(EXP)
+        elif bench == "kv":
+            run_kvbench(EXP)
 
 
 if __name__ == "__main__":
