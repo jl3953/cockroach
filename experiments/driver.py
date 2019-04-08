ed350f054e4df6cb2f3e4604dcfb148d8f64650c
Jeffrey Helt <jmhelt13@gmail.com>
2019-04-08 17:58:38 -0400

    Remove threading for now.
    Release note: None

--- experiments/driver.py
+++ experiments/driver.py
@@ -6,7 +6,6 @@ import os
 import subprocess
 import sys
 import time
-import threading
 
 # Constants
 COCKROACH_DIR = "/usr/local/temp/go/src/github.com/cockroachdb/cockroach"
@@ -112,14 +111,8 @@ def build_cockroach(nodes, commit):
            "&& (make build || (make clean && make build))") \
            .format(COCKROACH_DIR, commit)
 
-    ts = []
     for n in nodes:
-        t = threading.Thread(target=call_remote, args=(n["ip"], cmd, "Failed to build cockroach"))
-        t.start()
-        ts.append(t)
-
-    for t in ts:
-        t.join()
+        call_remote(n["ip"], cmd, "Failed to build cockroach")
     
 
 def init_experiment(config):
@@ -206,9 +199,9 @@ def main():
         if args.benchmark:
             for bench in args.benchmark:
                 if bench == "tpcc":
-                    run_tpcc(bench)
+                    run_tpcc(EXP)
                 elif bench == "kv":
-                    run_kvbench(bench)
+                    run_kvbench(EXP)
 
 if __name__ == "__main__":
     main()
