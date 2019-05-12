fb9911343aeee38f7167b1ad7e677d976b8b1109
root <root@node-0.jl87-qv52228.cops-pg0.utah.cloudlab.us>
2019-05-12 12:29:20 -0600

    Run 2 10 hot keys config

--- experiments/driver.py
+++ experiments/driver.py
@@ -107,9 +107,7 @@ EXP = {
     "benchmark": {
         "name": "kv",
         "init_args": {
-            "hot_keys": [
-                0
-            ]
+            "hot_keys": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         },
         "run_args": {
             "n_clients": 128,
