7875f96b9385dc68cf2ec7b51c9017daeb38e1c5
Jennifer Lam <jl87@princeton.edu>
2019-10-02 23:30:44 -0600

    change logging

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -3087,7 +3087,7 @@ func (s *Store) Send(
 			// this is the code path with the requesting client waiting.
 			if pErr.Index != nil {
 
-				log.Warningf(ctx, "jenndebug WriteIntentErr for txn:[%+v]", ba.Txn.TxnMeta.ID)
+				log.Warningf(ctx, "jenndebug WriteIntentErr for txn ba:[%+v]", ba)
 				var pushType roachpb.PushTxnType
 				if ba.IsWrite() {
 					pushType = roachpb.PUSH_ABORT
