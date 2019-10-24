978638a68537c49aa3923c2e8b75b0980a98a738
Jennifer Lam <jl87@princeton.edu>
2019-10-24 11:42:39 -0600

    Taking measurements for single key

--- experiments/driver.py
+++ experiments/driver.py
@@ -9,20 +9,30 @@ import os
 
 FPATH = os.path.dirname(os.path.realpath(__file__))
 EXP, SKEWS = exp_lib.create_experiment(FPATH, "default.ini")
+# CONFIG_LIST = [
+# 	"single_key_64.ini",
+# 	"single_key_72.ini",
+# 	"single_key_80.ini",
+# 	"single_key_96.ini",
+# 	]
 CONFIG_LIST = [
-	"default.ini",
-	"concurrency_8.ini",
+	"single_key_64.ini",
+	"single_key_64_30.ini",
+	# "single_key_64_60.ini",
 	]
 
-def run_experiment(exp, skews):
+def run_experiment(exp, skews, view=False):
 
 	exps = lib.vary_zipf_skew(exp, skews)
 	for e in exps:
 		lib.cleanup_previous_experiment(exp)
 		lib.init_experiment(exp)
 		lib.warmup_cluster(e)
-		lib.run_bench(e)
-	lib.gnuplot(exp, skews)
+		if not view:
+			lib.run_bench(e)
+
+	if not view:
+		lib.gnuplot(exp, skews)
 
 
 def main():
@@ -33,6 +43,7 @@ def main():
 	parser.add_argument('--benchmark', action='store_true', help='runs specified benchmark, assumes db is already started')
 	parser.add_argument('--override', action='store_true', help='overrides parameters according to override.ini,'
 			' only valid when running benchmark')
+	parser.add_argument('--view', action='store_true', help='only runs warmup for short testing')
 	# parser.add_argument('--logs', action='store_true', help='parses benchmark logs')
 	
 	args = parser.parse_args()
@@ -41,7 +52,7 @@ def main():
 	elif args.benchmark:
 		for config_file in CONFIG_LIST:
 			exp, skews = exp_lib.create_experiment(FPATH, config_file, args.override)
-			run_experiment(exp, skews)
+			run_experiment(exp, skews, args.view)
 	elif args.start:
 		lib.cleanup_previous_experiment(EXP)
 		lib.init_experiment(EXP)
