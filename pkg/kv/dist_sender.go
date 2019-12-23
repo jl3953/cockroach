ca97b661358d4061f6c599bfffa82340dd9721ed
Jennifer Lam <jl87@princeton.edu>
2019-12-22 21:24:52 -0700

    fix

--- pkg/kv/dist_sender.go
+++ pkg/kv/dist_sender.go
@@ -633,7 +633,7 @@ func splitBatchAndCheckForRefreshSpans(
 	for _, part := range parts {
 		for _, requestUnion := range part {
 			// if it's a write from me
-			if bytes.Equal(p.Key, []byte("0")) {
+			if bytes.Equal(requestUnion.GetInner().Header().Key, []byte("0")) {
 				hot = append(hot, requestUnion)
 			} else if _, ok := requestUnion.GetInner().(*roachpb.EndTransactionRequest); ok {
 				hot = append(hot, requestUnion)
