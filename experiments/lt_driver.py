762cb7859b4a68495d0204739983a30743b1e866
Jennifer Lam <jl87@princeton.edu>
2020-01-01 20:55:51 -0700

    latency throughput driver script

--- experiments/lt_driver.py
+++ experiments/lt_driver.py
@@ -9,14 +9,18 @@ import plotlib
 
 FPATH = os.path.dirname(os.path.realpath(__file__))
 
-def parse_config_file(config_file, variation_file):
-	exp, _ = exp_lib.create_experiment(FPATH, config_file)
-	variation_config = exp_lib.read_variation(variation_file)
+def parse_config_file(baseline_file, lt_file):
+	exp, _ = exp_lib.create_experiment(FPATH, baseline_file)
+	variation_config = exp_lib.read_variation(lt_file)
 
 	return exp, variation_config
 
 
-def find_optimal_parameters(exp, variations, view):
+def find_optimal_concurrency(exp, variations, log_output_path, is_view_only):
+
+	""" Returns:
+	Max concurrency, csv data
+	"""
 
 	start = variations["variation"]["concurrency"][0]
 	step_size = variations["variation"]["step_size"]
@@ -36,7 +40,7 @@ def find_optimal_parameters(exp, variations, view):
 				lib.cleanup_previous_experiment(exp)
 				lib.init_experiment(exp)
 				lib.warmup_cluster(e)
-				if not view:
+				if not is_view_only:
 					lib.run_bench(e)
 
 			datum = {"concurrency": concurrency}
@@ -50,32 +54,109 @@ def find_optimal_parameters(exp, variations, view):
 		end = concurrency + step_size
 		step_size = int(step_size / 2)
 
-	data = sorted(data, key=lambda i: i["concurrency"])
-	filename = plotlib.write_out_data(data, os.path.dirname(exp["out_dir"]))
-
-	driver_node = "192.168.1.1" # usually
-	csv_file = os.path.basename(os.path.dirname(exp["out_dir"])) + ".csv"
-	cmd = "mv {0} /usr/local/temp/go/src/github.com/cockroachdb/cockroach/gnuplot/{1}".format(filename, csv_file)
-	lib.call_remote(driver_node, cmd, "i like to move it move it")
-
-	return max_concurrency
+	
+	return max_concurrency, data
 
 
-def run_single_trial(config_file, variation_file, report_params_args, view):
+def report_csv_data(csv_data, args):
 
-	set_params, variations = parse_config_file(config_file, variation_file)
-	optimal_params = find_optimal_parameters(set_params, variations, view)
-	print(optimal_params)
-	# report_optimal_parameters(report_params_args)
+	""" Outputs csv data to file storage.
+
+	Args:
+		csv_data
+		args (dict): metadata for path of csv file, driver nodes, etc.
+
+	Returns:
+		None.
+
+	"""
+	data = sorted(csv_data, key=lambda i: i["concurrency"])
+	filename = plotlib.write_out_data(data, args["csv_output_path"], "lt.csv")
+
+
+def report_optimal_parameters(max_concurrency, args):
+
+	""" Outputs optimal concurrency to file storage.
+
+	Args:
+		max_concurrency (int)
+		args (dict): metadata for path of file, etc.
+
+	Returns:
+		None.
+
+	"""
+
+	filename = os.path.join(args["params_output_path"], "optimal_params.ini")
+	with open(filename, "w") as f:
+		f.write("max_concurrency: " + str(max_concurrency) + "\n")
+
+	
+
+def move_log_output(baseline_file, log_output_path, driver_node):
+
+	""" Moves log files from original location to given location.
+
+	Args:
+		baseline_file (str): name of baseline config file.
+		log_output_path (str): path of new location.
+		driver_node (str)
+
+	Returns:
+		None.
+
+	"""
+
+	log_dir = exp_lib.find_log_dir(FPATH, baseline_file)
+	
+	cmd = "mv {0} lt_output; mv lt_output {1}".format(log_dir, log_output_path)
+	lib.call_remote(driver_node, cmd, "mv lt_output err")
+
+
+def run_single_trial(find_concurrency_args, report_params_args,
+		report_csv_args, is_view_only):
+
+	print(find_concurrency_args)
+	set_params, variations = parse_config_file(find_concurrency_args["baseline_file"], 
+			find_concurrency_args["lt_file"])
+	max_concurrency, csv_data = find_optimal_concurrency(set_params,
+			variations, find_concurrency_args["log_output_path"], is_view_only)
+	# move_log_output(find_concurrency_args["baseline_file"],
+	# 		find_concurrency_args["log_output_path"], find_concurrency_args["driver_node"])
+	report_csv_data(csv_data, report_csv_args)
+	report_optimal_parameters(max_concurrency, report_params_args)
+	print(max_concurrency)
 
 
 def main():
 
 	parser = argparse.ArgumentParser(description="find latency throughput graph")
-	parser.add_argument('--view', action='store_true', help='only runs warmup for short testing')
+	parser.add_argument('baseline_file', help="baseline_file, original param file")
+	parser.add_argument('lt_file', help="lt_file, for example lt.ini")
+	parser.add_argument('log_output_path', help='path of log output files')
+	parser.add_argument('params_output_path', help="path of output param files")
+	parser.add_argument('csv_output_path', help="path of output csv file")
+	parser.add_argument('--driver_node', default='192.168.1.1')
+	parser.add_argument('--is_view_only', action='store_true', 
+			help='only runs warmup for short testing')
 	args = parser.parse_args()
 
-	run_single_trial("baseline_lt_2clients.ini", "lt.ini", "", args.view)
+	find_concurrency_args = {
+		"baseline_file": args.baseline_file,
+		"lt_file": args.lt_file,
+		"log_output_path": args.log_output_path,
+		"driver_node": args.driver_node,
+	}
+	report_csv_args = {
+		"csv_output_path": args.csv_output_path,
+	}
+
+	report_params_args = {
+		"params_output_path": args.params_output_path,
+	}
+
+	run_single_trial(find_concurrency_args, report_params_args, report_csv_args, 
+			args.is_view_only)
 
 
 if __name__ == "__main__":
